9a6fdc7c577ed0e86a6cf7757bd12ba94aa7e146
janpla <komunikacia.jajopi@gmail.com>
2020-10-08 19:57:38 +0200

    Impoved in so many ways

--- scripts.js
+++ scripts.js
@@ -1,23 +1,51 @@
 var images = [];
 var currentImage = 0;
 var control = 1;
-var help = "";
+var folder = "./data/";
+var descriptions = [];
 
+
+//Happens at the start
 $.ajax({
-    url : "./data",
+    url : folder,
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
             if (val.match(/\.(jpe?g|png|gif)$/) ) {
-                images.push(val);
-                //console.log(images);
+                images.push({photo: val.split("/").pop(), title: "", description: ""});
                 if(control === 1) {
-                    reloadImg();
+                    //Happens only once at start
+                    //reloadImg();
                     control = 0;
                 }
+            } else {
+                //readDescriptions("./data/descriptions.txt", reloadImg);
             }
         });
+        readDescriptions("./data/descriptions.txt");   
     }
 });
+
+function readDescriptions(file) {
+    $.ajax({url: file, success: function(result){
+        descriptions = result.replaceAll("\r","").split("\n");
+        //console.log(descriptions);
+        descriptions.forEach(i => {
+            images.forEach(j => {
+                var iHelp = i.split(": ");
+                if(j.photo.split(".")[0] === iHelp[0]) {
+                    if(iHelp[1] === "") {
+                        j.title = iHelp[0];
+                    } else {
+                        j.title = iHelp[1];
+                    }
+                    j.description = iHelp[2];
+                }
+            });
+        });
+        reloadImg();
+    }
+    })
+}
 
 function reloadImg(){
     if (currentImage < 0) {
@@ -26,9 +54,13 @@ function reloadImg(){
     if (currentImage > images.length - 1) {
         currentImage = 0;
     }
-    console.log(images[currentImage]);
+    //console.log(images[currentImage]);
     //$("#picture").attr("src", images[currentImage]);
-    $("#picDiv").css("background-image", "url("+images[currentImage]+")");
+    $("#picDiv").css("background-image", "url(./data/"+images[currentImage].photo+")");
+    console.log(images[currentImage].title);
+    $("#photoTitle").html(images[currentImage].title);
+    console.log(images[currentImage].description);
+    $("#photoDescription").html(images[currentImage].description);
 }
     
 $("#rightAr").click(() => {
